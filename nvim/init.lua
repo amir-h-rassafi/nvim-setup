3fa672e0e01aa9855e6be6dac474f255924f0465
amir <amir.h.rassafi@gmail.com>
2024-01-27 23:14:13 +0000

    New shortkey.

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,4 +18,5 @@ vim.cmd[[:set number]]
 
 
 vim.api.nvim_set_keymap('n', '<C-S>', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-F>', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
 
